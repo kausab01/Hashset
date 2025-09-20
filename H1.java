d2da710091d08248e4cafc1ad26f649aea0b5661
Kausab Badgujar <kausabbadgujar01@gmail.com>
2025-09-20 22:33:01 +0530

    first commit

---
+++ H1.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+
+public class H1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      HashSet<String> h=new  HashSet<String>();
+      
+      h.add("RED");
+      h.add("WHITE");
+      h.add("PINK ");
+      h.add("YELLOW");
+      h.add("BLACK");
+      h.add("GREEN");
+      
+      System.out.println("THE HASH SET : "+h);
+	}
+
+}
