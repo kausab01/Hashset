9b9a40306302a9598cb98200b0dd8b410ee810eb
Kausab Badgujar <kausabbadgujar01@gmail.com>
2025-09-21 16:03:54 +0530

    commit - h2

---
+++ H2.java
@@ -0,0 +1,21 @@
+import java.util.HashSet;
+
+public class H2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    HashSet<String> h=new  HashSet<String>();
+	      
+	      h.add("RED");
+	      h.add("WHITE");
+	      h.add("PINK ");
+	      h.add("YELLOW");
+	      h.add("BLACK");
+	      h.add("GREEN");
+	      
+	      for(String s: h){
+	    	  System.out.println(s);
+	      }
+	}
+
+}
