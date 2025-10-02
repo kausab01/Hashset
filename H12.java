5801a1606449f63c148b388c44641516c17f7dca
Kausab Badgujar <kausabbadgujar01@gmail.com>
2025-10-02 15:06:41 +0530

    commit - h12

---
+++ H12.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+
+public class H12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 HashSet<String> set = new HashSet<String>();
+	        set.add("RED");
+	        set.add("WHITE");
+	        
+	        set.add("PINK");
+	        set.add("GREY");
+
+	        System.out.println("HashSet  :" + set);
+	        set.clear();
+	        System.out.println("HashSet  :" + set);
+	}
+
+}
